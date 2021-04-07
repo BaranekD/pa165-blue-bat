da4bf76f5565724a7365a0d9436eaa4d0f71674d
rudolf <455952@mail.muni.cz>
2021-04-07 14:00:30 +0200

    Fix customer delete,fix Javadoc

--- src/main/java/cz/muni/fi/pa165/bluebat/entity/Customer.java
+++ src/main/java/cz/muni/fi/pa165/bluebat/entity/Customer.java
@@ -5,7 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 
+import javax.persistence.Entity;
 import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.GenerationType;
 import java.time.LocalDate;
 import java.util.Objects;
 
--- src/main/java/cz/muni/fi/pa165/bluebat/service/ExcursionServiceImpl.java
+++ src/main/java/cz/muni/fi/pa165/bluebat/service/ExcursionServiceImpl.java
@@ -4,6 +4,7 @@ import cz.muni.fi.pa165.bluebat.dao.ExcursionDao;
 import cz.muni.fi.pa165.bluebat.entity.Excursion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import org.springframework.transaction.annotation.Transactional;
 
 
