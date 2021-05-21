f856930472523045cd8e6a820d04574879014614
Dominik Baránek <0Baranek.dominik0@gmail.com>
2021-05-21 10:04:26 +0200

    Created REST API module

---
+++ travel-agency-rest/src/main/java/cz/muni/fi/pa165/bluebat/rest/ApiUris.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.bluebat.rest;
+
+public abstract class ApiUris {
+
+    public static final String ROOT_URI = "/pa165/rest";
+    public static final String ROOT_URI_RESERVATIONS = "/reservations";
+}
